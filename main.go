a1d94bb0b3ec1b9b85842d5ad886dfda0bddcd3e
enp0s10-GH <vroch30@gmail.com>
2023-01-24 21:51:43 +0100

    small fix

--- main.go
+++ main.go
@@ -15,28 +15,27 @@ const (
 )
 
 func main() {
-				service := AGENT_ADDR + ":" + AGENT_PORT
+		service := AGENT_ADDR + ":" + AGENT_PORT
 
-				// Creating Remote Address Information field + Error Handling
-				RemoteAddr, err := net.ResolveUDPAddr(AGENT_TYPE, service)
-				re.Encounter(re.ErrorInformation{ErrorType: "FATAL", Error: err})
+		// Creating Remote Address Information field + Error Handling
+		RemoteAddr, err := net.ResolveUDPAddr(AGENT_TYPE, service)
+		re.Encounter(re.ErrorInformation{ErrorType: "FATAL", Error: err})
 
-				// Dialing AGENT via UDP + Error Handling
-				conn, err := net.DialUDP(AGENT_TYPE, nil, RemoteAddr)
-				re.Encounter(re.ErrorInformation{ErrorType: "FATAL", Error: err})
-				defer conn.Close()
+		// Dialing AGENT via UDP + Error Handling
+		conn, err := net.DialUDP(AGENT_TYPE, nil, RemoteAddr)
+		re.Encounter(re.ErrorInformation{ErrorType: "FATAL", Error: err})
+		defer conn.Close()
 
-				// getting metric from cli argument and transmitting it to the client + Error Handling
-				input := strings.TrimSuffix(os.Args[1], "\n")
-				message := []byte(input)
-				_, err = conn.Write(message)
-				re.Encounter(re.ErrorInformation{ErrorType: "FATAL", Error: err})
+		// getting metric from cli argument and transmitting it to the client + Error Handling
+		input := strings.TrimSuffix(os.Args[1], "\n")
+		message := []byte(input)
+		_, err = conn.Write(message)
+		re.Encounter(re.ErrorInformation{ErrorType: "FATAL", Error: err})
 
-				// Creating Buffer and writing the response of the ROCKS Agent to it
-				buffer := make([]byte, 1024)
-				n, _, err := conn.ReadFromUDP(buffer)
+		// Creating Buffer and writing the response of the ROCKS Agent to it
+		buffer := make([]byte, 1024)
+		n, _, err := conn.ReadFromUDP(buffer)
 
-				//Printing the Metric Result
-				fmt.Println(string(buffer[:n]))
-
+		//Printing the Metric Result
+		fmt.Println(string(buffer[:n]))
 }
